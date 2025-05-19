dbee9b8d3fa5bce3cbad8d26e204133f3599bfb8
Gyubin <rbqks529@naver.com>
2025-05-19 20:23:59 +0900

    [Feat]-#11] - NavHost 추가 및 Auth와 Main으로 네비게이션 분기 처리

---
+++ app/src/main/java/com/mungkive/application/navigation/AuthNavGraph.kt
@@ -0,0 +1,40 @@
+package com.mungkive.application.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.mungkive.application.model.Routes
+import com.mungkive.application.ui.login.LoginView
+import com.mungkive.application.ui.login.WelcomeView
+
+@Composable
+fun AuthNavGraph(
+    navController: NavHostController,
+    onLoginSuccess: () -> Unit
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Routes.Welcome.route
+    ) {
+        composable(Routes.Welcome.route) {
+            WelcomeView(
+                /*onLoginClick = { navController.navigate(Routes.Login.route) }*/
+            )
+        }
+        composable(Routes.Login.route) {
+            LoginView(
+                /*onLoginSuccess = {
+                    onLoginSuccess() // 인증 성공 처리
+                }*/
+            )
+        }
+        composable(Routes.AddProfile.route) {
+            /*ProfileAddView(
+                onProfileSaved = {
+                    onLoginSuccess() // 최초 회원가입시 바로 인증 처리
+                }
+            )*/
+        }
+    }
+}
