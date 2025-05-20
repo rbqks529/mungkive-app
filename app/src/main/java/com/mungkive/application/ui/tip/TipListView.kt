65e42aa11c099df429e91ca5cfed97846c8674b3
Hoeun Lee <hoeunlee228@gmail.com>
2025-05-20 20:28:41 +0900

    [Feat] #19 - TipListView 구현

--- app/src/main/java/com/mungkive/application/ui/tip/TipListView.kt
+++ app/src/main/java/com/mungkive/application/ui/tip/TipListView.kt
@@ -1,15 +1,63 @@
 package com.mungkive.application.ui.tip
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mungkive.application.models.tip.TipDataFactory
 
 @Composable
 fun TipListView(modifier: Modifier = Modifier) {
-    Text(
-        text = "Tip List View",
+    val tips = TipDataFactory.getTips()
+
+    Column(
         modifier = modifier
             .fillMaxSize()
-    )
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = "팁",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp, bottom = 16.dp)
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth().weight(1f),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(bottom = 32.dp),
+        ) {
+            items(tips) {
+                TipView(
+                    title = it.title,
+                    image = it.image,
+                    content = it.content
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun TipListViewPreview() {
+    TipListView()
 }
