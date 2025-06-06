6a8bfe7de68a5e6f19bc3eec7aaaa06398e6a827
JoGyuBin <128724038+rbqks529@users.noreply.github.com>
2025-06-06 13:15:21 +0900

    [Network] ApiService 토큰 넣게 수정 [Network]-#48

--- app/src/main/java/com/mungkive/application/repository/PostRepository.kt
+++ app/src/main/java/com/mungkive/application/repository/PostRepository.kt
@@ -1,6 +1,7 @@
 package com.mungkive.application.repository
 
 import com.mungkive.application.network.ApiService
+import com.mungkive.application.network.dto.CommentResponse
 import com.mungkive.application.network.dto.PostCreateRequest
 import retrofit2.Response
 
@@ -8,9 +9,13 @@ class PostRepository(
     private val apiService: ApiService,
     private val token: String   // 하드코딩 토큰 사용
 ) {
-    suspend fun createPost(request: PostCreateRequest): Response<Void> {
 
-        // TODO: 헤더에 토큰 넣는거 뺴기 
+    // TODO: 헤더에 토큰 넣는거 뺴기
+    suspend fun createPost(request: PostCreateRequest): Response<Void> {
         return apiService.createPost("Bearer $token", request)
+    }
+
+    suspend fun getComments(postId: Long): List<CommentResponse> {
+        return apiService.getComments("Bearer $token", postId)
     }
 }
