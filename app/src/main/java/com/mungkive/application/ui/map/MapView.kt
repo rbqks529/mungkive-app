7b2e026ac23bdd6440e912a1e67c1564c82b265e
AJM500 <jmahn100@naver.com>
2025-06-09 21:16:04 +0900

    [Feat] #45 - 구조 변경 (상위 컴포져블 MapScreen에서 NaverMap 관리)

--- app/src/main/java/com/mungkive/application/ui/map/MapView.kt
+++ app/src/main/java/com/mungkive/application/ui/map/MapView.kt
@@ -1,59 +1,59 @@
 package com.mungkive.application.ui.map
 
-import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.mungkive.application.ui.feed.data.FeedData
+import com.mungkive.application.ui.map.Functions.toLatlng
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
-import com.naver.maps.map.compose.LocationTrackingMode
-import com.naver.maps.map.compose.MapProperties
+import com.naver.maps.map.compose.MapUiSettings
 import com.naver.maps.map.compose.NaverMap
 import com.naver.maps.map.compose.rememberCameraPositionState
-import kotlin.collections.getOrNull
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun MapView(
     modifier: Modifier = Modifier,
     feedList: List<FeedData>,
-    onFeedSelected: (String) -> Unit
+    onFeedSelected: (String) -> Unit,
+    onMapClick: () -> Unit
 ) {
     val context = LocalContext.current
-    // FeedData에 GPS 좌표 추가 (LatLng)
-    // 추후에 FeedData에 좌표값 추가되면 수정필요
-    val markerFeedList = listOf(
-        MapMarkerData(LatLng(37.5413, 127.0793), feed = feedList.getOrNull(0)),
-        MapMarkerData(LatLng(37.5435, 127.0774), feed = feedList.getOrNull(1)),
-        MapMarkerData(LatLng(37.5404, 127.0793), feed = null)
-    )
-
     // 네이버 맵 뷰 초기 위치 설정
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition(markerFeedList[0].position, 15.0)
+        // 카메라 위치를 가장 첫 게시물의 위치 기준으로 잡음.
+        position = CameraPosition(feedList[0]
+            .locate
+            .toLatlng()?:LatLng(37.5413, 127.0793),
+            15.0
+        )
     }
 
     NaverMap(
         modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-        // 카메라 설정 및 현재 위치 갱신 버튼
+        cameraPositionState = cameraPositionState,
+        onMapClick = { _,_ ->
+            onMapClick()
+        },
+        uiSettings = MapUiSettings(isLocationButtonEnabled = true)
+        // 현재 위치 맵에 띄우기
 //        properties = MapProperties(
-//            LocationTrackingMode.
+//            LocationTrackingMode.follow
 //        )
     ) {
-        markerFeedList.forEach { marker ->
-            val urlImg = marker.feed?.picture
-            if (urlImg != null) {
+        feedList.forEach { marker ->
+            val urlImg = marker.userPic
+            val location = marker.locate.toLatlng()
+            if (urlImg != null && location != null) {
                 MarkerWithUrlIcon(
-                    position = marker.position,
+                    position = location,
                     url = urlImg,
                     context = context,
                     onClick = {
-                        marker.feed?.let { onFeedSelected(it.id) }
+                        marker.let { onFeedSelected(it.id) }
                         true
                     }
                 )
