a696dd962180bce56a438a7330518bc9f49cc5dc
Gyubin <rbqks529@naver.com>
2025-06-10 01:15:59 +0900

    [Fix] 네이버 Reverse Geocoding 오류 수정

--- app/src/main/java/com/mungkive/application/ui/feed/map/NaverReverseGeocodeApi.kt
+++ app/src/main/java/com/mungkive/application/ui/feed/map/NaverReverseGeocodeApi.kt
@@ -1,5 +1,6 @@
 package com.mungkive.application.ui.feed.map
 
+import NaverReverseGeocodeResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
@@ -13,8 +14,8 @@ interface NaverReverseGeocodeApi {
     )
     fun reverseGeocode(
         @Query("coords") coords: String,
-        @Query("orders") orders: String = "roadaddr,legalcode,admcode,addr,roadaddr,land,region,site",
-        @Query("output") output: String = "json",
-        @Query("lang") lang: String = "ko"
+        @Query("orders") orders: String = "legalcode,admcode,addr,roadaddr",
+        @Query("output") output: String = "json"
     ): Call<NaverReverseGeocodeResponse>
+
 }
